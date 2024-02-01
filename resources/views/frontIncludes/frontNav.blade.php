1c38635079fe2e592c69e341b4b33c3a98fa466e
unknown <andrei.girnet96@gmail.com>
2024-02-01 17:21:24 +0200

    links

--- resources/views/frontIncludes/frontNav.blade.php
+++ resources/views/frontIncludes/frontNav.blade.php
@@ -1,16 +1,16 @@
 <div id="frontNav">
-    <a href="{{route('home')}}">
+    <a href="{{route('home')}}" class="whiteLink">
         <div id="mainLogo">
             <img src="{{asset("images/logo/logoWhite.png")}}" alt="">
         </div>
     </a>
     <div id="navMenu">
-        <a href="{{route('home')}}"><div class="navItem" x-text="data.navigation[0]">Home</div></a>
-        <a href="{{route('front.faq')}}"><div class="navItem" id="attentionItem" x-text="data.navigation[1]">Please Read Faq's</div></a>
+        <a href="{{route('home')}}" class="whiteLink"><div class="navItem" x-text="data.navigation[0]">Home</div></a>
+        <a href="{{route('front.faq')}}" class="whiteLink"><div class="navItem" id="attentionItem" x-text="data.navigation[1]">Please Read Faq's</div></a>
         {{--        <div class="navItem"><a href="{{route('front.products')}}">Online Courses</a></div>--}}
-        <a href="{{route('front.team')}}"><div class="navItem" x-text="data.navigation[2]">Team Training</div></a>
-        <a href="{{route('front.consulting')}}"><div class="navItem" x-text="data.navigation[3]">Consulting</div></a>
-        <a href="{{route('front.contact')}}"><div class="navItem" x-text="data.navigation[4]">Contact Us</div></a>
+        <a href="{{route('front.team')}}" class="whiteLink"><div class="navItem" x-text="data.navigation[2]">Team Training</div></a>
+        <a href="{{route('front.consulting')}}" class="whiteLink"><div class="navItem" x-text="data.navigation[3]">Consulting</div></a>
+        <a href="{{route('front.contact')}}" class="whiteLink"><div class="navItem" x-text="data.navigation[4]">Contact Us</div></a>
 {{--        <a href="{{route('front.blog')}}"><div class="navItem" x-text="data.navigation[5]">Blog</div></a>--}}
     </div>
 {{--    <div class="language-picker">--}}
@@ -30,8 +30,8 @@
         <div id="googleIcon">
             <img src="{{asset("images/logo/cpd-member-white.png")}}" alt="" id="cpd">
             <img src="{{asset("images/logo/rospa.png")}}" alt="" id="rospa">
-            <a href=""><img src="{{asset("images/logo/google-icon.webp")}}" alt=""></a>
+            <a href="" class="whiteLink"><img src="{{asset("images/logo/google-icon.webp")}}" alt=""></a>
         </div>
-        <a href="{{route("login")}}"><div class="navItem">Login</div></a>
+        <a href="{{route("login")}}" class="whiteLink"><div class="navItem">Login</div></a>
     </div>
 </div>
